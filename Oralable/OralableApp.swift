ce77fe42479cd5c8d051452a697ae6719c640602
John A Cogan <johnacogan67@gmail.com>
2024-09-10 10:32:17 +0100

    Initial Commit

---
+++ Oralable/OralableApp.swift
@@ -0,0 +1,17 @@
+//
+//  OralableApp.swift
+//  Oralable
+//
+//  Created by John A Cogan on 10/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct OralableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
